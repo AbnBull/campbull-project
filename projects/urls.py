a47733f7e86d9dc3bba3b87c938e56654e5b4825
AbnBull <davidaccampbell@gmail.com>
2019-05-07 21:45:57 -0500

    more work on projects

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.projectsmain, name='projectsmain'),
+    path('<int:project.id>/', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-
     return render(request, 'projects/home.html' )
 
 def projectsmain(request):
     projects = Project.objects
     return render(request, 'projects/projectsmain.html', {'projects':projects})
+
+def project_detail(request):
+    return render(request, 'projects/project_detail.html' )
